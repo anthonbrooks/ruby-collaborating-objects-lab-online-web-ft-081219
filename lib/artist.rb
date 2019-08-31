f60d4e280ec3cf89d72c1de7c7283be6cd548f2b
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-31 17:36:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,6 @@ class Artist
   #end 
   
   def self.find_or_create_by_name(artist_name)
-    if self.all.find { |artist| artist.name == artist_name }
       self.all.find { |artist| artist.name == artist_name }
     else
       self.new(artist_name)
